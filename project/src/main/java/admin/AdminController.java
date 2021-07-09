b081bb850e2fcf3b4262d2a8f4c4737c7a4edc69
user <eunssol01@gmail.com>
2021-07-09 17:06:46 +0900

    07.09 변경

---
+++ project/src/main/java/admin/AdminController.java
@@ -0,0 +1,24 @@
+package admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController {
+   @RequestMapping("/admin")
+   public String index() {
+      return "admin/index";
+   }
+   @RequestMapping("admin/board/index.do")
+   public String boardIndex() {
+      return "admin/board/index";
+   }
+   @RequestMapping("admin/board/view.do")
+   public String boardview() {
+      return "admin/board/view";
+   }
+   @RequestMapping("admin/board/write.do")
+   public String boardwrite() {
+      return "admin/board/write";
+   }
+}
